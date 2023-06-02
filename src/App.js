a9b5dcf40b23f2d3b421499354e88a1e50ef6d69
Sanjay-chandwani <sanjaychandwani91@gmail.com>
2023-06-02 20:00:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TouchTypingProject = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [randomSentence, setSentence] = useState("");
+    const [keyCount, setKeyCount] = useState(0);
+    const [keyCounted, setKeyCounted] = useState(false);
+    const [accuracy, setAccuracy] = useState(0);
 
-export default App;
+
+
+    function handleKeyCount() {
+        if (isCorrect && randomSentence.length === inputValue.length) {
+            setKeyCounted(true);
+            setInputValue("")
+            const newAccuracy = ((randomSentence.length / (keyCount + 1)) * 100).toFixed(2);
+            setAccuracy(newAccuracy);
+        }
+        setKeyCount(keyCount + 1);
+    }
+
+
+
+
+    const handleInputChange = (e) => {
+
+        setInputValue(e.target.value);
+    };
+    const sentences = [
+        "ladjasfj dlflkasd ",
+        "adfjkl asdfjkl",
+        "jladfsjlasd",
+        "dkfjsadfjkl",
+        "kasdfjklas",
+    ];
+
+    function getRandomSentence() {
+        const randomIndex = Math.floor(Math.random() * sentences.length);
+        setSentence(sentences[randomIndex]);
+      }
+      
+      useEffect(() => {
+        getRandomSentence();
+      }, []);
+      
+      function retryHandler() {
+        setKeyCount(0);
+        setKeyCounted(false);
+        setAccuracy(0);
+        getRandomSentence();
+      }
+      
+
+
+
+
+
+
+    const isCorrect = inputValue === randomSentence.slice(0, inputValue.length);
+
+    return (
+        <div id="wrapper">
+            <div className="touch-test">
+            <h2>Touch Typing Project</h2>
+            <p>Type the sentence below:</p>
+            <h2 style={{ color: isCorrect ? "green" : "red" }}>{randomSentence}</h2>
+            <textarea
+                value={inputValue}
+                onChange={handleInputChange} onKeyUp={handleKeyCount}
+                style={{
+                    backgroundColor: isCorrect ? "lightgreen" : "pink",
+                    color: "black",
+                }}
+            ></textarea>
+
+            {
+                keyCounted ? (<div><p>keyCount : {keyCount}</p>  <p>Accuracy : {accuracy}</p> <button onClick={() => {retryHandler()}}>Retry</button> </div>) : ("")
+            }
+            </div>
+            
+        </div>
+        
+    );
+};
+
+export default TouchTypingProject;
